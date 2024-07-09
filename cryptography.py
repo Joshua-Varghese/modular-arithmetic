65400098e308e8c8289c08e4b62e833ea10f3f61
Joshua-Varghese <joshuadude2715@gmail.com>
2024-07-09 23:47:07 +0530

    To science and research, and for God almighty

---
+++ cryptography.py
@@ -0,0 +1,13 @@
+
+class Algorithms:
+    def __init__():
+        pass
+
+    def modular_exponentiation(self,base,x,n):
+        pass
+    
+    def modular_division(self,a,b,n):
+        pass
+
+if '__name__' == '__main__':
+    return False
